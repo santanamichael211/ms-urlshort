e458a7c3edf19d508330e3f6421cd94d83646bdd
Glitch (ms-urlshort) <none>
2018-05-16 16:04:08 +0000

    👧🍛 Checkpoint ./server.js:1118247/345 ./package.json:1118247/20

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-// server.js
-// where your node app starts
+
 
 // init project
 var express = require('express');
 var app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+var mongo = require('mongodb').MongoClient;
+
+
+
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
